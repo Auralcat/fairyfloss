d5c9c121ecc115083631f85d1cc4c4a0d18358de
Auralcat <ashleyzt@hi2.in>
2019-03-27 20:21:35 -0300

    Add faces for ace-window and ace-jump

--- fairyfloss-theme.el
+++ fairyfloss-theme.el
@@ -91,105 +91,113 @@ Authors:
 
    ;; Typography faces
    `(default ((t (:inherit nil :stipple nil :background "#5A5475" :foreground ,fairy-space-100 :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120))))
-  `(fixed-pitch ((t (:family "Monospace"))))
-  `(variable-pitch ((t (:family "Sans Serif"))))
+   `(fixed-pitch ((t (:family "Monospace"))))
+   `(variable-pitch ((t (:family "Sans Serif"))))
 
-  ;; Company-mode
-  `(company-preview ((t (:foreground "#2aa198" :background "#eee8d5"))))
-  `(company-preview-search ((((background light)) (:inherit (company-tooltip-common-selection))) (((background dark)) (:background "blue1" :inherit (company-preview)))))
-  `(company-tooltip ((t (:background "#39354b" :foreground ,fairy-sky-500))))
-  `(company-tooltip-common ((t (:underline (:color foreground-color :style line) :foreground "#5fafd7" :background "#5f5f5f"))))
-  `(company-tooltip-common-selection ((t (:underline (:color foreground-color :style line) :foreground "#586e75"))))
-  `(company-tooltip-selection ((t (:background "#626262" :foreground "#afd7ff"))))
+   ;; Company-mode
+   `(company-preview ((t (:foreground "#2aa198" :background "#eee8d5"))))
+   `(company-preview-search ((((background light)) (:inherit (company-tooltip-common-selection))) (((background dark)) (:background "blue1" :inherit (company-preview)))))
+   `(company-tooltip ((t (:background "#39354b" :foreground ,fairy-sky-500))))
+   `(company-tooltip-common ((t (:underline (:color foreground-color :style line) :foreground "#5fafd7" :background "#5f5f5f"))))
+   `(company-tooltip-common-selection ((t (:underline (:color foreground-color :style line) :foreground "#586e75"))))
+   `(company-tooltip-selection ((t (:background "#626262" :foreground "#afd7ff"))))
 
-  ;; Enh-ruby mode
-  `(enh-ruby-heredoc-delimiter-face ((t (:inherit enh-ruby-string-delimiter-face))))
-  `(enh-ruby-op-face ((t (:foreground ,fairy-blush-300))))
-  `(enh-ruby-regexp-delimiter-face ((t (:inherit enh-ruby-string-delimiter-face))))
-  `(enh-ruby-regexp-face ((t (:foreground ,fairy-grape-500))))
-  `(enh-ruby-string-delimiter-face ((t (:inherit font-lock-string-face))))
+   ;; Enh-ruby mode
+   `(enh-ruby-heredoc-delimiter-face ((t (:inherit enh-ruby-string-delimiter-face))))
+   `(enh-ruby-op-face ((t (:foreground ,fairy-blush-300))))
+   `(enh-ruby-regexp-delimiter-face ((t (:inherit enh-ruby-string-delimiter-face))))
+   `(enh-ruby-regexp-face ((t (:foreground ,fairy-grape-500))))
+   `(enh-ruby-string-delimiter-face ((t (:inherit font-lock-string-face))))
 
-  ;; Font-lock-mode which is used for general programming faces
-  `(font-lock-builtin-face ((t (:foreground ,fairy-grape-500))))
-  `(font-lock-comment-delimiter-face ((t (:inherit (font-lock-comment-face)))))
-  `(font-lock-comment-face ((t (:foreground ,fairy-gold-800))))
-  `(font-lock-constant-face ((t (:foreground ,fairy-grape-500))))
-  `(font-lock-doc-face ((t (:inherit font-lock-string-face))))
-  `(font-lock-function-name-face ((t (:foreground ,fairy-mint-500))))
-  `(font-lock-keyword-face ((t (:foreground ,fairy-blush-300))))
-  `(font-lock-negation-char-face ((t nil)))
-  `(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
-  `(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
-  `(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
-  `(font-lock-string-face ((t (:foreground ,fairy-gold-500))))
-  `(font-lock-type-face ((t (:foreground ,fairy-mint-500 :slant italic))))
-  `(font-lock-variable-name-face ((t (:foreground ,fairy-gold-400))))
-  `(font-lock-warning-face ((t nil)))
+   ;; Font-lock-mode which is used for general programming faces
+   `(font-lock-builtin-face ((t (:foreground ,fairy-grape-500))))
+   `(font-lock-comment-delimiter-face ((t (:inherit (font-lock-comment-face)))))
+   `(font-lock-comment-face ((t (:foreground ,fairy-gold-800))))
+   `(font-lock-constant-face ((t (:foreground ,fairy-grape-500))))
+   `(font-lock-doc-face ((t (:inherit font-lock-string-face))))
+   `(font-lock-function-name-face ((t (:foreground ,fairy-mint-500))))
+   `(font-lock-keyword-face ((t (:foreground ,fairy-blush-300))))
+   `(font-lock-negation-char-face ((t nil)))
+   `(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
+   `(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
+   `(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
+   `(font-lock-string-face ((t (:foreground ,fairy-gold-500))))
+   `(font-lock-type-face ((t (:foreground ,fairy-mint-500 :slant italic))))
+   `(font-lock-variable-name-face ((t (:foreground ,fairy-gold-400))))
+   `(font-lock-warning-face ((t nil)))
 
-  ;; IDO
-  `(ido-only-match ((t (:foreground "color-84"))))
-  `(ido-subdir ((t (:foreground "color-210"))))
+   ;; IDO
+   `(ido-only-match ((t (:foreground "color-84"))))
+   `(ido-subdir ((t (:foreground "color-210"))))
 
-  ;; Isearch
-  `(isearch ((((class color) (min-colors 88) (background light)) (:foreground "lightskyblue1" :background "magenta3")) (((class color) (min-colors 88) (background dark)) (:foreground "brown4" :background "palevioletred2")) (((class color) (min-colors 16)) (:foreground "cyan1" :background "magenta4")) (((class color) (min-colors 8)) (:foreground "cyan1" :background "magenta4")) (t (:inverse-video t))))
-  `(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "RosyBrown1")) (((class color) (min-colors 88) (background dark)) (:background "red4")) (((class color) (min-colors 16)) (:background "red")) (((class color) (min-colors 8)) (:background "red")) (((class color grayscale)) (:foreground "grey")) (t (:inverse-video t))))
+   ;; Isearch
+   `(isearch ((((class color) (min-colors 88) (background light)) (:foreground "lightskyblue1" :background "magenta3")) (((class color) (min-colors 88) (background dark)) (:foreground "brown4" :background "palevioletred2")) (((class color) (min-colors 16)) (:foreground "cyan1" :background "magenta4")) (((class color) (min-colors 8)) (:foreground "cyan1" :background "magenta4")) (t (:inverse-video t))))
+   `(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "RosyBrown1")) (((class color) (min-colors 88) (background dark)) (:background "red4")) (((class color) (min-colors 16)) (:background "red")) (((class color) (min-colors 8)) (:background "red")) (((class color grayscale)) (:foreground "grey")) (t (:inverse-video t))))
 
-  ;; Js2-mode
-  `(js2-function-param ((t (:foreground ,fairy-carrot-1000))))
-  `(lazy-highlight ((((class color) (min-colors 88) (background light)) (:background "paleturquoise")) (((class color) (min-colors 88) (background dark)) (:background "paleturquoise4")) (((class color) (min-colors 16)) (:background "turquoise3")) (((class color) (min-colors 8)) (:background "turquoise3")) (t (:underline (:color foreground-color :style line)))))
-  `(link ((t (:underline (:color foreground-color :style line) :foreground "cyan1"))))
-  `(link-visited ((t (:foreground "violet" :inherit (link)))))
-  `(linum ((t (:foreground ,fairy-space-100))))
-  `(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground ,fairy-space-100 :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
-  `(minibuffer-prompt ((t (:foreground ,fairy-space-100))))
+   ;; Js2-mode
+   `(js2-function-param ((t (:foreground ,fairy-carrot-1000))))
+   `(lazy-highlight ((((class color) (min-colors 88) (background light)) (:background "paleturquoise")) (((class color) (min-colors 88) (background dark)) (:background "paleturquoise4")) (((class color) (min-colors 16)) (:background "turquoise3")) (((class color) (min-colors 8)) (:background "turquoise3")) (t (:underline (:color foreground-color :style line)))))
+   `(link ((t (:underline (:color foreground-color :style line) :foreground "cyan1"))))
+   `(link-visited ((t (:foreground "violet" :inherit (link)))))
+   `(linum ((t (:foreground ,fairy-space-100))))
+   `(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground ,fairy-space-100 :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
+   `(minibuffer-prompt ((t (:foreground ,fairy-space-100))))
 
-  ;; Mode-line
-  `(mode-line ((t (:box (:color nil :style released-button) :foreground ,fairy-space-1000 :background ,fairy-space-300))))
-  `(mode-line-buffer-id ((t (:foreground "color-161" :weight bold))))
-  `(mode-line-emphasis ((t (:weight bold))))
-  `(mode-line-highlight ((((class color) (min-colors 88)) (:box (:color "grey40" :style released-button))) (t (:inherit (highlight)))))
-  `(mode-line-inactive ((t (:weight light :box (:color "grey40" :style nil) :foreground "grey80" :background "grey30" :inherit (mode-line)))))
-  `(next-error ((t (:inherit (region)))))
+   ;; Mode-line
+   `(mode-line ((t (:box (:color nil :style released-button) :foreground ,fairy-space-1000 :background ,fairy-space-300))))
+   `(mode-line-buffer-id ((t (:foreground "color-161" :weight bold))))
+   `(mode-line-emphasis ((t (:weight bold))))
+   `(mode-line-highlight ((((class color) (min-colors 88)) (:box (:color "grey40" :style released-button))) (t (:inherit (highlight)))))
+   `(mode-line-inactive ((t (:weight light :box (:color "grey40" :style nil) :foreground "grey80" :background "grey30" :inherit (mode-line)))))
+   `(next-error ((t (:inherit (region)))))
 
-  ;; Org-agenda
-  `(org-agenda-dimmed-todo-face ((t (:foreground "gold4"))))
-  `(org-agenda-date ((t (:inherit font-lock-function-name-face))))
-  `(org-agenda-date-weekend ((t (:foreground "violet"))))
-  `(org-agenda-structure ((t (:foreground "#EFDCCD"))))
-  `(org-agenda-structure ((t (:foreground "#d5b568"))))
-  `(org-upcoming-deadline ((t (:foreground "#B1AA98"))))
+   ;; Org-agenda
+   `(org-agenda-dimmed-todo-face ((t (:foreground "gold4"))))
+   `(org-agenda-date ((t (:inherit font-lock-function-name-face))))
+   `(org-agenda-date-weekend ((t (:foreground "violet"))))
+   `(org-agenda-structure ((t (:foreground "#EFDCCD"))))
+   `(org-agenda-structure ((t (:foreground "#d5b568"))))
+   `(org-upcoming-deadline ((t (:foreground "#B1AA98"))))
 
-  ;; General Org faces
-  `(org-date ((t (:inherit font-lock-variable-name-face))))
-  `(org-scheduled-previously ((t (:foreground "#778855"))))
-  `(org-scheduled-previously ((t (:inherit (font-lock-comment-face)))))
-  `(org-scheduled-today ((t (:inherit (font-lock-type-face)))))
-  `(org-table ((t (:foreground "color-159"))))
-  `(org-todo ((t (:foreground "#FF857F" :weight bold))))
-  `(query-replace ((t (:inherit (isearch)))))
+   ;; General Org faces
+   `(org-date ((t (:inherit font-lock-variable-name-face))))
+   `(org-scheduled-previously ((t (:foreground "#778855"))))
+   `(org-scheduled-previously ((t (:inherit (font-lock-comment-face)))))
+   `(org-scheduled-today ((t (:inherit (font-lock-type-face)))))
+   `(org-table ((t (:foreground "color-159"))))
+   `(org-todo ((t (:foreground "#FF857F" :weight bold))))
+   `(query-replace ((t (:inherit (isearch)))))
 
-  ;; Org levels
-  `(org-level-1 ((t (:foreground ,fairy-gold-300))))
-  `(org-level-2 ((t (:foreground ,fairy-carrot-300))))
-  `(org-level-3 ((t (:foreground ,fairy-blush-500))))
-  `(org-level-4 ((t (:foreground ,fairy-mint-500))))
-  `(org-level-5 ((t (:foreground ,fairy-sky-500))))
-  `(org-level-6 ((t (:foreground ,fairy-grape-300))))
-  `(org-level-7 ((t (:foreground ,fairy-gold-800))))
+   ;; Org levels
+   `(org-level-1 ((t (:foreground ,fairy-gold-300))))
+   `(org-level-2 ((t (:foreground ,fairy-carrot-300))))
+   `(org-level-3 ((t (:foreground ,fairy-blush-500))))
+   `(org-level-4 ((t (:foreground ,fairy-mint-500))))
+   `(org-level-5 ((t (:foreground ,fairy-sky-500))))
+   `(org-level-6 ((t (:foreground ,fairy-grape-300))))
+   `(org-level-7 ((t (:foreground ,fairy-gold-800))))
 
-  ;; Org-pomodoro
-  `(org-pomodoro-mode-line ((t (:foreground ,fairy-carrot-1000))))
-  `(org-pomodoro-mode-line-break ((t (:foreground ,fairy-mint-800))))
+   ;; Org-pomodoro
+   `(org-pomodoro-mode-line ((t (:foreground ,fairy-carrot-1000))))
+   `(org-pomodoro-mode-line-break ((t (:foreground ,fairy-mint-800))))
 
-  ;; Rainbow Delimiters
-  `(rainbow-delimiters-depth-1-face ((t (:inherit org-level-1))))
-  `(rainbow-delimiters-depth-2-face ((t (:inherit org-level-2))))
-  `(rainbow-delimiters-depth-3-face ((t (:inherit org-level-3))))
-  `(rainbow-delimiters-depth-4-face ((t (:inherit org-level-4))))
-  `(rainbow-delimiters-depth-5-face ((t (:inherit org-level-5))))
-  `(rainbow-delimiters-depth-6-face ((t (:inherit org-level-6))))
-  `(rainbow-delimiters-depth-7-face ((t (:inherit org-level-7))))
-  `(rainbow-delimiters-depth-8-face ((t (:inherit org-level-8)))))
+   ;; Ace-window
+   `(aw-background-face    ((t (:foreground  ,fairy-space-600))))
+   `(aw-leading-char-face  ((t (:foreground  ,fairy-carrot-700))))
+
+   ;; Ace-jump
+   `(ace-jump-face-background ((t (:inherit aw-background-face))))
+   `(ace-jump-face-foreground ((t (:inherit aw-leading-char-face))))
+
+   ;; Rainbow Delimiters
+   `(rainbow-delimiters-depth-1-face ((t (:inherit org-level-1))))
+   `(rainbow-delimiters-depth-2-face ((t (:inherit org-level-2))))
+   `(rainbow-delimiters-depth-3-face ((t (:inherit org-level-3))))
+   `(rainbow-delimiters-depth-4-face ((t (:inherit org-level-4))))
+   `(rainbow-delimiters-depth-5-face ((t (:inherit org-level-5))))
+   `(rainbow-delimiters-depth-6-face ((t (:inherit org-level-6))))
+   `(rainbow-delimiters-depth-7-face ((t (:inherit org-level-7))))
+   `(rainbow-delimiters-depth-8-face ((t (:inherit org-level-8)))))
 
   )
 
